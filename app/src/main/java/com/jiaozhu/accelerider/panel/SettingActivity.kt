a197621dd14e5bf4f4be6572d57d806e51be0398
q4232134 <195313059@qq.com>
2017-10-25 17:36:20 +0800

    修复部分bug，增加帮助与关于

--- app/src/main/java/com/jiaozhu/accelerider/panel/SettingActivity.kt
+++ app/src/main/java/com/jiaozhu/accelerider/panel/SettingActivity.kt
@@ -1,11 +1,16 @@
 package com.jiaozhu.accelerider.panel
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.preference.PreferenceActivity
 import android.preference.PreferenceFragment
+import android.support.v7.app.AlertDialog
 import android.view.MenuItem
+import android.view.View
 import com.jiaozhu.accelerider.R
 import com.jiaozhu.accelerider.support.Preferences
+import com.jiaozhu.accelerider.support.Tools
+import toast
 
 /**
  * Created by jiaozhu on 2017/9/6.
@@ -18,14 +23,46 @@ class SettingActivity : BaseSettingsActivity() {
         loadHeadersFromResource(R.xml.pref_headers, target)
     }
 
+
+    fun click(view: View) {
+        showSaveDialog(view.tag.toString())
+
+    }
+
+    private fun showSaveDialog(tag: String) {
+        val build = AlertDialog.Builder(this)
+        build.setTitle("保存二维码")
+        build.setMessage("是否保存${tag}二维码？")
+        build.setNegativeButton("取消", null)
+        build.setPositiveButton("保存") { _, _ ->
+            when (tag) {
+                "微信" -> {
+                    val temp = BitmapFactory.decodeResource(resources, R.drawable.wx)
+                    temp?.let {
+                        Tools.saveBitmap(it, Preferences.DownloadPath, "微信二维码.png")
+                        toast("微信二维码已保存到默认下载目录")
+                    }
+                }
+                "支付宝" -> {
+                    val temp = BitmapFactory.decodeResource(resources, R.drawable.zfb)
+                    temp?.let {
+                        Tools.saveBitmap(it, Preferences.DownloadPath, "支付宝二维码.jpg")
+                        toast("支付宝二维码已保存到默认下载目录")
+                    }
+                }
+            }
+        }
+        build.create().show()
+    }
+
     /**
-     * 构建fragment与数据绑定
+     * 设定fragment
      */
     class GeneralPreferenceFragment : PreferenceFragment() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             //设定内容
-            addPreferencesFromResource(R.xml.preference)
+            addPreferencesFromResource(R.xml.setting_preference)
             //绑定Preference
             preferenceManager.sharedPreferencesName = Preferences.SHAREDPREFERENCES_NAME
             setHasOptionsMenu(true)
@@ -37,12 +74,44 @@ class SettingActivity : BaseSettingsActivity() {
 
     }
 
+
+    /**
+     * 帮助fragment
+     */
+    class HelpPreferenceFragment : PreferenceFragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            //设定内容
+            addPreferencesFromResource(R.xml.help_preference)
+            //绑定Preference
+            preferenceManager.sharedPreferencesName = Preferences.SHAREDPREFERENCES_NAME
+            setHasOptionsMenu(true)
+        }
+    }
+
+
+    /**
+     * 帮助fragment
+     */
+    class AboutPreferenceFragment : PreferenceFragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            //设定内容
+            addPreferencesFromResource(R.xml.about_preference)
+            //绑定Preference
+            preferenceManager.sharedPreferencesName = Preferences.SHAREDPREFERENCES_NAME
+            setHasOptionsMenu(true)
+        }
+    }
+
     /**
      * 固定结构
      */
     override fun isValidFragment(fragmentName: String): Boolean =
             PreferenceFragment::class.java.name == fragmentName ||
-                    GeneralPreferenceFragment::class.java.name == fragmentName
+                    GeneralPreferenceFragment::class.java.name == fragmentName ||
+                    HelpPreferenceFragment::class.java.name == fragmentName ||
+                    AboutPreferenceFragment::class.java.name == fragmentName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +119,6 @@ class SettingActivity : BaseSettingsActivity() {
     }
 
     private fun setupActionBar() {
-        val actionBar = actionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
